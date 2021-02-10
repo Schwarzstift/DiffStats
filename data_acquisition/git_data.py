39a7aa1d5e619e5c6f7d06ce06d51418dff1cf88
chris <chris.carstensen@gmail.com>
2021-02-10 22:19:57 +0100

    Change in test data

--- data_acquisition/git_data.py
+++ data_acquisition/git_data.py
@@ -2,6 +2,10 @@ from git import Repo
 import os
 
 
+def get_repository(path):
+    return Repo(path)
+
+
 def iterate_over_tree(tree, files):
     for entry in tree.blobs:
         files.append(entry.path)
@@ -9,13 +13,20 @@ def iterate_over_tree(tree, files):
         iterate_over_tree(subtree, files)
 
 
-def get_tracked_files(path):
-    repo = Repo(path)
+def get_head_ref(repo: Repo):
+    return repo.head.ref
+
+
+def files_touched_between(repo: Repo, ref_before:str, ref_after:str):
+    tree_before = repo.commit(ref_before).tree
+
+    tree_after = repo.commit(ref_after).tree
+
+
+def get_tracked_files(repo: Repo, ref:str):
     files = []
-
-    tree = repo.head.commit.tree
+    tree = repo.commit(ref).tree
     iterate_over_tree(tree, files)
-
     return files
 
 
